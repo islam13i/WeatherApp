7e9eced79b715f8b1a1e6024b43fe89aed4e54af
IslamB <islam.baigaziev13@google.com>
2020-03-14 14:37:08 +0600

    create project

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Islam on 3/14/20.
+//  Copyright © 2020 Islam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
